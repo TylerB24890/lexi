7f40e64350a6a7baf73fea21609c1222bd34aab3
Tyler Bailey <tyler@elexicon.com>
2018-12-13 13:06:46 -0500

    Add Lexi version # to options pages.

--- admin/partials/admin-dashboard.php
+++ admin/partials/admin-dashboard.php
@@ -1,5 +1,8 @@
 <div id="lexi" class="wrap">
-  <h1><?php _e('Welcome to the Lexi Framework!', 'lexi'); ?></h1>
+  <h1>
+    <?php _e('Welcome to the Lexi Framework!', 'lexi'); ?>
+    <small style="font-size: 14px; display: block;"><?php printf(__('Version %s', 'lexi'), LEXI_VERSION); ?></small>
+  </h1>
 
   <div class="content">
     <?php _e('A simple, yet robust developer framework for building high quality enterprise WordPress websites and applications.', 'lexi'); ?>
--- admin/partials/admin-options.php
+++ admin/partials/admin-options.php
@@ -1,3 +1,11 @@
 <div id="lexi" class="wrap">
-  <h1><?php _e('Lexi Framework Options', 'lexi'); ?></h1>
+  <h1>
+    <?php _e('Lexi Framework Options', 'lexi'); ?>
+    <small style="font-size: 14px; display: block;"><?php printf(__('Version %s', 'lexi'), LEXI_VERSION); ?></small>
+  </h1>
+
+  <div class="content">
+    <h3>Add Post Type, Taxonomy and Sidebar generator fields</h3>
+    <h3>Add custom 'parts' directory name (?)</h3>
+  </div>
 </div>
